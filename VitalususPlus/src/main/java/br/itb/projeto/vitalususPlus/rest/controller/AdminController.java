d3df5ceacc313ca9dd1c8b352ab11aefe88617b8
Otto Fidelis <ottofidelis@gmail.com>
2024-05-06 10:37:29 -0300

    só reformatei a classe AdminController mesmo

--- VitalususPlus/src/main/java/br/itb/projeto/vitalususPlus/rest/controller/AdminController.java
+++ VitalususPlus/src/main/java/br/itb/projeto/vitalususPlus/rest/controller/AdminController.java
@@ -28,36 +28,42 @@ public class AdminController {
         this.adminService = adminService;
         this.usuarioService = usuarioService;
     }
+
     @GetMapping("findAll")
-    public ResponseEntity<List<Admin>> findAll(){
+    public ResponseEntity<List<Admin>> findAll() {
         List<Admin> administradores = this.adminService.findAll();
         return new ResponseEntity<List<Admin>>(administradores, HttpStatus.OK);
     }
+
     @GetMapping("findById/{id}")
-    public ResponseEntity<Admin> findById(@PathVariable long id){
+    public ResponseEntity<Admin> findById(@PathVariable long id) {
         Admin admin = this.adminService.findById(id);
         return new ResponseEntity<Admin>(admin, HttpStatus.OK);
     }
+
     @PostMapping("post")
-    public ResponseEntity<Admin> salvarAdmin(@RequestBody @Valid Admin admin){
+    public ResponseEntity<Admin> salvarAdmin(@RequestBody @Valid Admin admin) {
         Admin adminSalvo = this.adminService.save(admin);
         return new ResponseEntity<Admin>(adminSalvo, HttpStatus.OK);
     }
+
     @DeleteMapping("delete")
-    public void deletarAdmin(@RequestBody @Valid Admin admin){
+    public void deletarAdmin(@RequestBody @Valid Admin admin) {
         adminService.delete(admin);
     }
+
     @PutMapping("update")
-    public ResponseEntity<Admin> updateAdmin(@RequestBody @Valid Admin admin){
+    public ResponseEntity<Admin> updateAdmin(@RequestBody @Valid Admin admin) {
         Admin adminUpdatado = this.adminService.update(admin);
         return new ResponseEntity<Admin>(adminUpdatado, HttpStatus.OK);
     }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> erro = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach(error ->{
-            String fieldName = ((FieldError)error).getField();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
             String erroMessage = error.getDefaultMessage();
             erro.put(fieldName, erroMessage);
         });
