cb1c03766684a21a4daf9f8ee558073ac83e13b1
AndreyCsilva2006 <andreycsilvadev@gmail.com>
2024-06-21 19:27:24 -0300

    -- Andrey -- Mobile Versão a1.5.0 Conexão com o Banco para fazer uma consulta de alguma tabela, atualmente apenas de Aluno, mas no futuro vai ser mudada para a de Treinador. otimizações da conexão do banco. bugfix.

--- Mobile/Vitalusus+/app/src/main/java/com/br/projeto/vitalusus/dao/AlunoDAO.java
+++ Mobile/Vitalusus+/app/src/main/java/com/br/projeto/vitalusus/dao/AlunoDAO.java
@@ -6,6 +6,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.br.projeto.vitalusus.conexao.Conexao;
 import com.br.projeto.vitalusus.model.Aluno;
@@ -19,14 +20,106 @@ import java.util.List;
 
 // DAO - Data Access Object (Objeto de Acesso de Dados)
 public class AlunoDAO {
+    // preparando conexão
+    private Connection conn = null;
 
-//    public Aluno cadastrarAluno(String nome, String email, String senha){
-//
-//    }
+    public void cadastrar(Aluno a){
+        try {
+            executeSql("insert into Aluno (nome, email, senha) values ('"+a.getNome()+"', '"+a.getEmail()+"', '"+a.getSenha()+"')");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void alterar(Aluno a){
+        try {
+            executeSql("update Aluno set Nome = '"+a.getNome()+"', Email = '"+a.getEmail()+"', Senha = '"+a.getSenha()+"' where id = "+a.getId());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void excluir(Aluno a){
+        try {
+            executeSql("delete from Aluno where id = "+a.getId());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Aluno findById(Integer id){
+        List<Aluno> lista = new ArrayList<Aluno>();
+        try {
+            conn = Conexao.conectar();
+            if(conn != null){
+                String sql = "select * from Aluno where id = "+id;
+
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(sql);
+                while(rs.next()){
+                    Aluno alu = new Aluno();
+                    alu.setId(rs.getInt(1));
+                    alu.setNome(rs.getString(2));
+                    alu.setEmail(rs.getString(3));
+                    alu.setSenha(rs.getString(4));
+//                    alu.setpSeguranca(rs.getString(5));
+//                    alu.setrSeguranca(rs.getString(6));
+
+                    conn.close();
+                    return alu;
+                }
+
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Log.e("Erro no AlunoDAO.java no método findById(Integer id)", e.getMessage());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            Log.e("Erro no AlunoDAO.java no método findById(Integer id)", throwables.getMessage());
+        }
+
+        return null;
+    }
+
+    // Método para executar comandos SQL, com abertura e fechamento do banco.
+    private  void executeSql(String sql) throws SQLException, ClassNotFoundException {
+            conn = Conexao.conectar();
+            if(conn != null){
+                Statement st = conn.createStatement();
+                st.executeQuery(sql);
+                conn.close();
+            }
+    }
+
+    private List<Aluno> getAluno(String sql) throws SQLException {
+        List<Aluno> lista = new ArrayList<Aluno>();
+
+        Statement st = null;
+        st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        while(rs.next()){
+            Aluno alu = new Aluno();
+            alu.setId(rs.getInt(1));
+            alu.setNome(rs.getString(2));
+            alu.setEmail(rs.getString(3));
+            alu.setSenha(rs.getString(4));
+//                    alu.setpSeguranca(rs.getString(5));
+//                    alu.setrSeguranca(rs.getString(6));
+
+            lista.add(alu);
+        }
+        return lista;
+    }
 
     public Aluno selecionarAluno(String email, String senha){
         try {
-            Connection conn = Conexao.conectar();
+            conn = Conexao.conectar();
             if(conn != null){
                 String sql = "select * from Aluno where email = '"+email+"' and senha = '"+senha+"'";
                 Statement st = null;
@@ -51,41 +144,46 @@ public class AlunoDAO {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-
         return null;
     }
 
     public List<Aluno> getAll(){
         List<Aluno> lista = new ArrayList<Aluno>();
         try {
-            Connection conn = Conexao.conectar();
+            conn = Conexao.conectar();
             if(conn != null){
                 String sql = "select * from Aluno";
-                Statement st = null;
-                st = conn.createStatement();
+                lista = getAluno(sql);
 
-                ResultSet rs = st.executeQuery(sql);
-                while(rs.next()){
-                    Aluno alu = new Aluno();
-                    alu.setId(rs.getInt(1));
-                    alu.setNome(rs.getString(2));
-                    alu.setEmail(rs.getString(3));
-                    alu.setSenha(rs.getString(4));
-//                    alu.setpSeguranca(rs.getString(5));
-//                    alu.setrSeguranca(rs.getString(6));
-
-                    lista.add(alu);
-                }
                 conn.close();
-
             }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+            Log.e("Erro no AlunoDAO.java no método getAll", e.getMessage());
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+            Log.e("Erro no AlunoDAO.java no método getAll", throwables.getMessage());
         }
-
         return lista;
     }
 
+    public List<Aluno> getAll(String busca){
+        List<Aluno> lista = new ArrayList<Aluno>();
+        try {
+            conn = Conexao.conectar();
+            if(conn != null){
+                String sql = "select * from Aluno where nome like '%"+busca+"%'";
+                lista = getAluno(sql);
+
+                conn.close();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            Log.e("Erro no AlunoDAO.java no método getAll(String busca)", e.getMessage());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            Log.e("Erro no AlunoDAO.java no método getAll(String busca)", throwables.getMessage());
+        }
+        return lista;
+    }
 }
--- Mobile/Vitalusus+/app/src/main/java/com/br/projeto/vitalusus/view/ListarAlunos.java
+++ Mobile/Vitalusus+/app/src/main/java/com/br/projeto/vitalusus/view/ListarAlunos.java
@@ -2,10 +2,16 @@ package com.br.projeto.vitalusus.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.br.projeto.vitalusus.R;
+import com.br.projeto.vitalusus.adapter.ListaAlunoAdapter;
 import com.br.projeto.vitalusus.dao.AlunoDAO;
 import com.br.projeto.vitalusus.model.Aluno;
 
@@ -13,22 +19,46 @@ import java.util.List;
 
 public class ListarAlunos extends AppCompatActivity {
 
-    TextView txtConsultaAlunoNome;
+    EditText editPesquisa;
+    Button btnPesquisar, btnCadastro;
+    ListView listView;
+    ListaAlunoAdapter adapter;
+    List<Aluno> lista;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_alunos);
 
-        txtConsultaAlunoNome = findViewById(R.id.txtConsultaAlunoNome);
+        editPesquisa = findViewById(R.id.editConsultaAlunoPesquisa);
+        btnPesquisar = findViewById(R.id.btnConsultaAlunoPesquisar);
+        btnCadastro = findViewById(R.id.btnConsultaAlunoCadastro);
+        listView = findViewById(R.id.listViewConsultaAluno);
 
-        preenche();
+        preenche("");
+
+        btnPesquisar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                preenche(editPesquisa.getText().toString());
+            }
+        });
     }
 
-    private void preenche() {
+    private void preenche(String busca) {
         AlunoDAO dao = new AlunoDAO();
-        List<Aluno> lista = dao.getAll();
-        for (Aluno aluno : lista){
-            txtConsultaAlunoNome.setText(aluno.getNome());
+        // busca pesquisar
+        if (busca.isEmpty()){
+            lista = dao.getAll();
         }
+        else{
+            lista = dao.getAll(busca);
+        }
+
+
+        adapter = new ListaAlunoAdapter(lista, this);
+        listView.setAdapter(adapter);
     }
+
+
 }
