63953ec9320307ed8ec48e89f10d036a635a6ea1
slaqlqrcoisa <carlosnt254@gmail.com>
2024-07-21 19:11:14 -0300

    -- Carlos -- Mobile Versão a1.8.32
    -Home sendo ainda desenvolvida, Fragments e suas funcionalidades criadas
    -Nova opção no drawer, o Suporte
    -2 arquivos deletados, sendo esses arquivos versões anteriores do home do Drey

--- Mobile/Vitalusus+/app/src/main/java/com/br/projeto/vitalusus/HomeActivity.java
+++ Mobile/Vitalusus+/app/src/main/java/com/br/projeto/vitalusus/HomeActivity.java
@@ -1,14 +1,121 @@
 package com.br.projeto.vitalusus;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
+    FragmentManager fragmentManager;
+    Toolbar toolbar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        
+        NavigationView navigationView = findViewById(R.id.navigation_drawer);
+        navigationView.setNavigationItemSelectedListener(null);
+        
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setBackground(null);
+        
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.bottom_home){
+                    openFragment(new HomeFragment());
+                    return true;
+                } else if (itemId == R.id.bottom_canais) {
+                    openFragment(new CanaisFragment());
+                    return true;
+                } else if (itemId == R.id.bottom_notificacoes) {
+                    openFragment(new NotificacoesFragment());
+                    return true;
+                } else if (itemId == R.id.bottom_perfil) {
+                    openFragment(new PerfilFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+        
+        fragmentManager = getSupportFragmentManager();
+        openFragment(new HomeFragment());
     }
+
+    // Funcionalidade de selecionar cada Fragment
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.nav_videosalta){
+            openFragment(new VideosAltaFragment());
+        } else if (itemId == R.id.nav_suporte) {
+            openFragment(new SuporteFragment());
+        } else if  (itemId == R.id.nav_configuracoes){
+            Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.nav_estatisticas){
+            Toast.makeText(this, "Estatisticas", Toast.LENGTH_SHORT).show();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        //Caso ter Estatisticas e configurações como um dos Fragments -
+        // } else if (itemId == R.id.nav_estatisticas) {
+        //   openFragment(new EstatisticasFragment());
+        // } else if (itemId == R.id.nav_configuracoes) {
+        //  openFragment(new ConfiguracoesFragment());
+        // }
+
+
+
+        return true;
+    }
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+
+        }
+        super.onBackPressed();
+    }
+
+    private void openFragment(Fragment fragment){
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
+    }
+
+
+
+
 }
