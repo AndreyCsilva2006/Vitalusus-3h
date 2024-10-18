3628e71a9d4d514b5d12eda2a87b280772b2ba30
OttoFidelis <ottofidelis@gmail.com>
2024-10-18 16:23:16 -0300

    sdfsdf

--- VitalususPlus/src/main/java/br/itb/projeto/vitalususPlus/service/UsuarioService.java
+++ VitalususPlus/src/main/java/br/itb/projeto/vitalususPlus/service/UsuarioService.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.stereotype.Service;
 
 import br.itb.projeto.vitalususPlus.model.entity.Usuario;
